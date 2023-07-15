c8eaf619551da394bc9b412d8d382f04c217a3b6
Gkicks <>
2023-07-15 14:02:48 +0000

    add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,6 +74,8 @@ function calculateCorrectAnswer() {
 
     if (operator === '+') {
         return [operand1 + operand2, 'addition'];
+    } else if (operator === '-') {
+        return [operand1 - operand2, 'subtract'];
     } else if (operator === 'x') {
         return [operand1 * operand2, 'multiply'];
     } else {
@@ -113,8 +115,8 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion(operand1, operand2) {
 
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = '-';
 
 }
